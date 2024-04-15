254e17455148559afaa93ec7214977367745ef84
son7877 <hongbeom7877@naver.com>
2024-04-16 02:10:10 +0900

    search_bar

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <>
-      <form>
-        <input className="" type="text" placeholder="레시피입력..." />
-        <button type="submit">검색</button>
-      </form>
-    </>
+    <div className="flex w-full rounded">
+      <input type="text" placeholder="레시피입력..." />
+      <button
+        className="m-2 rounded bg-green-600 px-10 py-2 text-white hover:text-yellow-300"
+        type="submit"
+      >
+        검색
+      </button>
+    </div>
   );
 };
 export default SearchBar;
