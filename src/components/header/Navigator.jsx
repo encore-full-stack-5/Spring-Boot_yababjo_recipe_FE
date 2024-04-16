589584192406af0ec5f9d3cadabb395e486d96b9
son7877 <hongbeom7877@naver.com>
2024-04-16 13:44:17 +0900

    header수정

--- src/components/header/Navigator.jsx
+++ src/components/header/Navigator.jsx
@@ -4,7 +4,7 @@ const Navigator = () => {
   return (
     <>
       <div className="flex flex-row items-center gap-x-4 h-13 overflow-visible bg-green-600">
-        <ul className="flex flex-wrap text-sm font-medium text-center text-white w-full">
+        <ul className="flex flex-wrap text-center text-white w-full">
           <li className="flex-1">
             <button className="w-full px-4 py-3 rounded-lg text-2xl  hover:text-yellow-300">
               추천
