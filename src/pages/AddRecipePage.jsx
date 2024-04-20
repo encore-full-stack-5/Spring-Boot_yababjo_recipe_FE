14d0b01111f6057020509acce1a69a3b4d34d66f
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-20 17:32:55 +0900

    Spring 연동

--- src/pages/AddRecipePage.jsx
+++ src/pages/AddRecipePage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function AddRecipePage() {
-  return <div>레시피 추가 페이지</div>;
-}
+const AddRecipePage = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/v1/comments/recipe/1"
+        );
+        if (response.status === 200) {
+          alert("API 호출 성공!");
+          console.log(response.data);
+        } else {
+          alert("API 호출 실패!");
+        }
+      } catch (error) {
+        console.error("API 호출 중 오류 발생:", error);
+        alert("API 호출 중 오류 발생!");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>리액트 컴포넌트</div>;
+};
 
 export default AddRecipePage;
